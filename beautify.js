edc5fddd6b68e4b13deb3fc8a963c71df7d707ee
JayLee <jzwaylove@gmail.com>
2019-02-13 11:36:09 +0900

    morning

---
+++ beautify.js
@@ -0,0 +1,4 @@
+alert("Hello");
+function helloWorld(a) {
+    console.log(a);
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+const _ = require('underscore');
+const log = console.log;
+var a = 10;
+var a = 20;
+let b = 10;
+const c = ["A","B","C"];
+//c = ["A","B"];
+c.push("d");
+
+console.log(_.first(c));
